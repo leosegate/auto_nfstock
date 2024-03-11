4b6dd2de6c839844d15e7a087bb5568296a2aa97
Leonardo Segate <66527512+leosegate@users.noreply.github.com>
2024-03-11 17:48:36 -0300

    Add files via upload

---
+++ auto_nfstock.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from os import listdir
+from os.path import isfile, join
+from pynput.keyboard import Key, Controller
+import time
+import re
+import webbrowser
+
+keyboard = Controller()
+
+#diretorio da pasta onde estao os xml's
+path_of_xmls = input("Digite o diretorio dos cte que deseja verificar, obs: colocar 2 barras: \n")
+
+#pega todos os nomes dos arquivos e joga pra uma lista
+all_xmls = [file for file in listdir(path_of_xmls) if isfile(join(path_of_xmls, file))]
+
+i=0
+while (i<len(all_xmls)): 
+    with open(path_of_xmls+"\\"+all_xmls[i], 'r') as file:
+        data = file.read()
+
+    all_data = BeautifulSoup(data, "xml")
+
+    #procura no arquivo apenas a tag que tem o cte
+    cte_number = str(all_data.find_all('nCT'))
+
+    #pega apenas o que esta dentro da tag
+    cte_number = re.findall('\d+', cte_number)
+    cte_number = cte_number[0]
+    
+    print(cte_number)
+    
+    #aqui vai o link do nfstock + a variavel cte number
+    webbrowser.open('https://www.google.com/')
+    time.sleep(5)
+    
+    #fecha a aba do nfstock sozinho
+    with keyboard.pressed(Key.ctrl):
+        keyboard.press('w')
+        keyboard.release('w')     
+    
+    if(i == len(all_xmls)-1): 
+        break
+    else:
+        print("proximo cte:")
+    i+=1
+
+print("\nTotal de ctes:", i+1)
+end_program = input("Verificacao concluida. Aperte enter para sair.")
