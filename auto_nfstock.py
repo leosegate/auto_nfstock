111873d84cee91edaf7feba25e787b84809fd4c2
Leonardo Segate <66527512+leosegate@users.noreply.github.com>
2024-03-13 16:01:27 -0300

    Update auto_nfstock.py
    botao de confirmacao(".") para o proximo cte adicionado

--- auto_nfstock.py
+++ auto_nfstock.py
@@ -2,18 +2,26 @@ from bs4 import BeautifulSoup
 from os import listdir
 from os.path import isfile, join
 from pynput.keyboard import Key, Controller
+import keyboard
 import time
 import re
 import webbrowser
 
-keyboard = Controller()
+def confirm_to_next():
+    while True:
+        if keyboard.read_key() == '.':
+            time.sleep(0.5)
+            break;
+
+
+teclado = Controller()
 
 input("Para funcionar, logue no nfstock primeiro e depois pressione ENTER aqui nesta tela.")
 
-#diretorio da pasta onde estao os xml's
+# diretorio da pasta onde estao os xml's
 path_of_xmls = input("\n\n\nDigite o diretorio dos cte que deseja verificar: \n")
 
-#pega todos os nomes dos arquivos e joga pra uma lista
+# pega todos os nomes dos arquivos e joga pra uma lista
 all_xmls = [file for file in listdir(path_of_xmls) if isfile(join(path_of_xmls, file))]
 
 i=0
@@ -23,23 +31,24 @@ while (i<len(all_xmls)):
 
     all_data = BeautifulSoup(data, "xml")
 
-    #procura no arquivo apenas a tag que tem o cte
+    # procura no arquivo apenas a tag que tem o cte
     cte_number = str(all_data.find_all('nCT'))
 
-    #pega apenas o que esta dentro da tag
+    # pega apenas o que esta dentro da tag
     cte_number = re.findall('\d+', cte_number)
     cte_number = cte_number[0]
     
     print(cte_number)
     
-    #aqui vai o link do nfstock + a variavel cte_number
+    # aqui vai o link do nfstock + a variavel cte_number
     webbrowser.open("https://nfstock.alterdata.com.br/Cte/ContratadosFiltro?OrdernarPor=0&MostrarOpcaoFiltroImportacao=True&Modelo=&Serie=&Numero="+cte_number+"&Chave=&StatusManifestacao=-1&Tipo=Tomador&DataDe=&DataAte=")
-    time.sleep(5)
     
-    #fecha a aba do nfstock sozinho
-    with keyboard.pressed(Key.ctrl):
-        keyboard.press('w')
-        keyboard.release('w')     
+    confirm_to_next()
+
+    # fecha a aba do nfstock sozinho
+    with teclado.pressed(Key.ctrl):
+        teclado.press('w')
+        teclado.release('w')     
     
     if(i == len(all_xmls)-1): 
         break
